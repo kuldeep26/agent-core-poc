eafa7e1bfa08304edfb0cb7208dd8c9cb55a2a59
Kuldeep Singh <kuldeepsingh260893@gmail.com>
2026-03-24 11:54:40 +0000

    fixing iam issue

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -29,3 +29,69 @@ resource "aws_iam_role_policy" "bedrock_policy" {
     ]
   })
 }
+
+resource "aws_iam_role" "lambda_role" {
+  name = "agent-core-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "agent-core-lambda-policy"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:*"
+        ],
+        Resource = "*"
+      },
+
+      {
+        Effect = "Allow",
+        Action = [
+          "sagemaker:*"
+        ],
+        Resource = "*"
+      },
+
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:*"
+        ],
+        Resource = "*"
+      },
+
+      {
+        Effect = "Allow",
+        Action = [
+          "ce:GetCostAndUsage"
+        ],
+        Resource = "*"
+      },
+
+      {
+        Effect = "Allow",
+        Action = [
+          "sts:AssumeRole"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
