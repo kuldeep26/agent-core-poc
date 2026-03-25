7ac07ae32f238eb8f4a6b6085815bffdce30db1f
Kuldeep Singh <kuldeepsingh260893@gmail.com>
2026-03-25 12:51:20 +0000

    fixing teams notification logic

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,6 +5,7 @@ variable "region" {
 variable "teams_webhook" {
   description = "Microsoft Teams Webhook URL for notifications"
   type        = string
+  default     = "https://outlook.office.com/webhook/your-webhook-url"
 }
 
 
